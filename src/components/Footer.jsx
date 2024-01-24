1f9167219078411bd3a62f1d4145fb363b30ed8d
eftekher-alam <eftekher@yahoo.com>
2024-01-24 11:50:39 +0600

    All done

---
+++ src/components/Footer.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <>
+      <div className="footer bg-[#171717]">
+        <div className="wrap flex justify-between items-center px-12 text-white font-bold py-6 tl:px-4 tl:py-4 ">
+          <h1 className="text-3xl">Eftekher</h1>
+          <div className="flex gap-4 cursor-pointer ">
+            <a href="https://www.facebook.com/eftekher2018/" target="_blank">
+              <i className="fa-brands fa-facebook border-[1px] border-white p-2 rounded-[100%] hover:bg-white hover:text-black"></i>
+            </a>
+            <a href="https://www.linkedin.com/in/eftekher/" target="_blank">
+              <i className="fa-brands fa-linkedin border-[1px] border-white p-2 rounded-[100%] hover:bg-white hover:text-black"></i>
+            </a>
+            <a href="https://github.com/eftekher-alam" target="_blank">
+              <i className="fa-brands fa-github border-[1px] border-white p-2 rounded-[100%] hover:bg-white hover:text-black"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Footer;
